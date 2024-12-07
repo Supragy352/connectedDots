40abe703dc02df615ac1627e964784e8b2c61f66
mishra03supragya@gmail.com <mishra03supragya@gmail.com>
2024-12-08 02:41:19 +0530

    working random movement

--- src/main.c
+++ src/main.c
@@ -20,22 +20,28 @@ void DrawDot(Dot *dot) {
     DrawCircle(dot->x, dot->y, dot->radius, dot->color);
 }
 
-// void connectDots(Dot *dot) {
-//     Dot *a = dot;
-//     Dot *b = dot + 1;
-
-//     DrawLine(a->x, a->y, b->x, b->y, WHITE);
-// }
-
 int calculateDistance(Dot* dot1, Dot* dot2) {
     return sqrt(pow(abs((dot1->x - dot2->x)), 2) + pow(abs((dot1->y - dot2->y)), 2));
 }
 
-void connectDots(Dot* dot1, Dot* dot2) {
-    if (calculateDistance(dot1, dot2) <= 300) {
-        DrawLine(dot1->x, dot1->y, dot2->x, dot2->y, WHITE);
-    }
+void connectDots(Dot *dot) {
+    Dot *dot1 = dot;
+    Dot *dot2 = dot + 1;
+    if (calculateDistance(dot1, dot2) <= 250) {
+       DrawLine(dot1->x, dot1->y, dot2->x, dot2->y, WHITE);
+     }
 }
+
+void MoveDot(Dot *dot) {
+    dot->x += (rand() % 11) - 5;
+    dot->y += (rand() % 11) - 5;
+}
+
+// void connectDots(Dot* dot1, Dot* dot2) {
+//     if (calculateDistance(dot1, dot2) <= 300) {
+//         DrawLine(dot1->x, dot1->y, dot2->x, dot2->y, WHITE);
+//     }
+// }
 
 
 //-------------------------------------------------------------------------
@@ -45,30 +51,27 @@ int main(int argc, char const *argv[])
 {
     srand(time(NULL));
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Dots");
-    SetTargetFPS(165);
-    // Dot d[20] = {};
+    SetTargetFPS(60);
 
-    // for (int i = 0; i < 20; i++) {
-    //     d[i].x = rand() % 400 + 1;
-    //     d[i].y = rand() % 400 + 1;
-    //     d[i].radius = 3;
-    //     d[i].color = WHITE;
-    // }
+    Dot d[20] = {};
+
+    for (int i = 0; i < 20; i++) {
+        d[i].x = rand() % 400 + 1;
+        d[i].y = rand() % 400 + 1;
+        d[i].radius = 3;
+        d[i].color = WHITE;
+    }
+
 
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-            // for (int i = 0; i < 20; ++i)
-            // {   
-            //     DrawDot(&d[i]);
-            //     connectDots(&d[i]);
-            // }
-
-            Dot a = {0, 0, 3, WHITE};
-            Dot b = {GetMouseX(), GetMouseY(), 3, WHITE};
-            // Dot b = {GetMouseX(), GetMouseY(), 3, WHITE};
-            connectDots(&a, &b);
-
+            for (int i = 0; i < 20; ++i)
+            {
+                DrawDot(&d[i]);
+                MoveDot(&d[i-1]);
+                connectDots(&d[i]);
+            }
         EndDrawing();
     }
     return 0;
